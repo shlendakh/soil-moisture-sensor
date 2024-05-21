6dec756213cde774542947d1c8ef70ba345df8ce
Paweł Szlendak <kontakt@pawelszlendak.pl>
2024-05-21 15:02:09 +0200

    less power consumption (device only publish value to mqtt server)

--- extra_script.py
+++ extra_script.py
@@ -13,14 +13,24 @@ load_dotenv(dotenv_path)
 wifi_ssid = os.getenv('WIFI_SSID')
 wifi_password = os.getenv('WIFI_PASSWORD')
 
+mqtt_server = os.getenv('MQTT_SERVER')
+mqtt_topic = os.getenv('MQTT_TOPIC')
+mqtt_user = os.getenv('MQTT_USER')
+mqtt_password = os.getenv('MQTT_PASSWORD')
+
 # Add Build Flags
-if wifi_ssid and wifi_password:
+if wifi_ssid and wifi_password and mqtt_server and mqtt_topic and mqtt_user and mqtt_password:
     print(f'Using WIFI_SSID={wifi_ssid} and WIFI_PASSWORD={wifi_password}')
+    print(f'Using MQTT_SERVER={mqtt_server} and MQTT_TOPIC={mqtt_topic}')
     env.Append(
         BUILD_FLAGS=[
             f'-DWIFI_SSID=\\"{wifi_ssid}\\"',
-            f'-DWIFI_PASSWORD=\\"{wifi_password}\\"'
+            f'-DWIFI_PASSWORD=\\"{wifi_password}\\"',
+            f'-DMQTT_SERVER=\\"{mqtt_server}\\"',
+            f'-DMQTT_TOPIC=\\"{mqtt_topic}\\"',
+            f'-DMQTT_USER=\\"{mqtt_user}\\"',
+            f'-DMQTT_PASSWORD=\\"{mqtt_password}\\"'
         ]
     )
 else:
-    print('WIFI_SSID or WIFI_PASSWORD is not set in .env file')
+    print('WIFI_SSID or WIFI_PASSWORD or MQTT SETTINGS is not set in .env file')
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <Arduino.h>
+/*
+  The code is pretty simple. It reads the value from the soil moisture sensor, calculates the percentage of moisture, and publishes it to the MQTT broker. 
+  The  .env  file is used to store the sensitive information like the WiFi SSID, password, MQTT server, user, and password.
+
+  On other branch of project, we have code with async server installed on ESP8266, but I resigned from it, because it was too much power consuming.
+
+  Author: Paweł Szlendak
+  Github: https://github.com/shlendakh
+  License: MIT (c) 2024
+*/
+
 #include <ESP8266WiFi.h>
-#include <ESPAsyncWebServer.h>
-#include <LittleFS.h>
+#include <PubSubClient.h>
 
 #define SOIL_MOISTURE_PIN A0
 
@@ -9,62 +18,69 @@
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
-AsyncWebServer server(80);  // Server instance
+const char *mqtt_server = MQTT_SERVER;
+const char *mqtt_user = MQTT_USER;
+const char *mqtt_password = MQTT_PASSWORD;
+const char *mqtt_topic = MQTT_TOPIC;
 
-// Function to read a file from LittleFS
-String readFile(fs::FS &fs, const char * path) {
-  File file = fs.open(path, "r");
-  if(!file || file.isDirectory()) {
-    Serial.println("- failed to open file for reading");
-    return String();
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define SOIL_MOISTURE_PIN A0
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  
-  String fileContent;
-  while(file.available()) {
-    fileContent += String((char)file.read());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   }
-  file.close();
-  return fileContent;
 }
 
 void setup() {
   Serial.begin(115200);
-  pinMode(SOIL_MOISTURE_PIN, INPUT);
-
-  // Initialize LittleFS
-  if (!LittleFS.begin()) {
-    Serial.println("Error mounting LittleFS");
-    return;
-  }
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-  Serial.println(WiFi.localIP());
-
-  // Serve HTML file
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    String html = readFile(LittleFS, "/index.html");
-    //String html = "<html><head><title>Test</title></head><body><p>Test2</p></body></html>";
-    request->send(200, "text/html", html);
-  });
-
-  server.on("/percents", HTTP_GET, [](AsyncWebServerRequest *request) {
-    int soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
-    float moisturePercent = 100 * (1 - (soilMoistureValue / 1023.0));
-
-    request->send(200, "text/plain", String(moisturePercent));
-  });
-
-  server.serveStatic("/assets/", LittleFS, "/assets/");
-
-  server.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  // Do nothing here
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  int soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
+  float moisturePercent = 100 * (1 - (soilMoistureValue / 1023.0));
+
+  char msg[50];
+  snprintf(msg, 50, "{\"moisture\": %.2f}", moisturePercent);
+  Serial.print("Publishing: ");
+  Serial.println(msg);
+  client.publish(mqtt_topic, msg);
+
+  delay(5000);
 }
